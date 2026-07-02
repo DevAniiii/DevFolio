f16c822a43ac39257c77d0e42a6a556dbfc18fd3
Anirban Banerjee <anirbanbanerjeeofficial@outlook.com>
2026-07-02 20:17:28 +0530

    feat: Added META DATA for SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,13 @@ import Contact from "@/components/footer/Contact";
 
 export default function Home() {
   return (
-    <>
-      <div className="relative min-h-screen">
-        <Hero />
-        <Experience />
-        <TechStack />
-        <About />
-        <Projects />
-        <Contact />
-      </div>
-    </>
+    <main className="relative min-h-screen">
+      <Hero />
+      <Experience />
+      <TechStack />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
   );
 }
